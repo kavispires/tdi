72f57867066b40c02ae468cf4f9d9b4b0c33a732
kavispires <kavispires@gmail.com>
2021-12-10 20:01:32 -0800

    Added resources

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Available</h1>
+        <ul>
+          <li>dc: b1, b2</li>
+          <li>dx: dd, hm, mm, mr, od, or, rv</li>
+          <li>mu: aw</li>
+          <li>my: bs, pr</li>
+          <li>us</li>
+        </ul>
       </header>
     </div>
   );
