6db47c0122d18b19927f1cf576c09307c23725f6
kavispires <kavispires@gmail.com>
2023-01-22 10:07:57 -0800

    Updated data/names for us characters

--- src/pages/SuspectsPage.tsx
+++ src/pages/SuspectsPage.tsx
@@ -1,37 +1,56 @@
-import { Image, Typography, Layout } from 'antd';
+import { Image, Layout, message } from 'antd';
 import { Fragment } from 'react';
 import { useCardWidth } from 'hooks/useCardWidth';
+import { useAsync, useCopyToClipboard } from 'react-use';
+import { ManOutlined, WomanOutlined } from '@ant-design/icons';
 
 const { Header, Content } = Layout;
 
 const DECK = Array(114).fill(1);
 
 export function SuspectCards() {
-  // const qp = useQueryParams({ deck: 'd1' });
-  // const [current, setCurrent] = useState('');
+  const [, copyToClipboard] = useCopyToClipboard();
   const cardWidth = useCardWidth();
+  const [messageApi, contextHolder] = message.useMessage();
 
-  // const onClick: MenuProps['onClick'] = (e) => {
-  //   qp.add('deck', e.key);
-  // };
+  const { loading, value } = useAsync(async () => {
+    const response = await fetch(`${process.env.PUBLIC_URL}/data/us/info.json`);
+    const result = await response.json();
+    return result;
+  }, []);
 
-  // useEffect(() => {
-  //   setCurrent(qp.queryParams.deck);
-  // }, [qp.queryParams]);
+  const info = (url: string) => {
+    copyToClipboard(url);
+    messageApi.info(`Copied ${url}`);
+  };
 
   return (
     <Layout className="page" id="page">
       <Header className="page__header" style={{ paddingInline: 0 }}>
-        <h1>Suspects</h1>
+        <h1>Suspects</h1> {loading} {contextHolder}
       </Header>
-      <Content className="page__content">
+      <Content className="page__content suspects">
         <Image.PreviewGroup>
           {DECK.fill(1).map((e, i) => {
             const id = e + i < 10 ? `0${e + i}` : `${e + i}`;
-
+            const entryId = `us-${id}`;
+            const data = Boolean(value) ? value[entryId] : null;
+            const url = `${process.env.PUBLIC_URL}/images/us/${id}.jpg`;
+            const url2 = `https://www.kavispires.com/tdi/images/us/${id}.jpg`;
             return (
               <Fragment key={`img-${id}`}>
-                <Image width={cardWidth} src={`${process.env.PUBLIC_URL}/images/us/${id}.jpg`} placeholder />
+                <div className="suspect" style={{ width: `${cardWidth}px` }}>
+                  <Image width={cardWidth} src={url} placeholder className="suspect__image" />
+                  {Boolean(data) && (
+                    <span className="suspect__name" role="button" onClick={() => info(url2)}>
+                      {data.gender === 'name' ? <ManOutlined /> : <WomanOutlined />}
+                      <br />
+                      🇧🇷 {data.name.pt}
+                      <br />
+                      🇺🇸 {data.name.en}
+                    </span>
+                  )}
+                </div>
                 {/* <Image
                     key={`img-${id}`}
                     width={cardWidth}
