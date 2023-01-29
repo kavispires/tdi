2cd6fd618010a92b450da64294e21fd1a35113a6
kavispires <kavispires@gmail.com>
2023-01-28 17:10:47 -0800

    [us] Added AI generated images

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { HashRouter, Route, Routes } from 'react-router-dom';
 import { Home } from 'pages/Home';
 import { ImageCards } from 'pages/ImageCardsPage';
 import { SuspectCards } from 'pages/SuspectsPage';
+import { Prompter } from 'pages/PrompterPage';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/image-cards" element={<ImageCards />} />
         <Route path="/suspects" element={<SuspectCards />} />
+        <Route path="/prompter" element={<Prompter />} />
       </Routes>
     </HashRouter>
   );
--- src/pages/SuspectsPage.tsx
+++ src/pages/SuspectsPage.tsx
@@ -1,17 +1,22 @@
-import { Image, Layout, message } from 'antd';
+import { Image, Layout, message, Select } from 'antd';
 import { Fragment } from 'react';
 import { useCardWidth } from 'hooks/useCardWidth';
 import { useAsync, useCopyToClipboard } from 'react-use';
 import { ManOutlined, WomanOutlined } from '@ant-design/icons';
+import { useQueryParams } from 'hooks/useQueryParams';
 
 const { Header, Content } = Layout;
 
-const DECK = Array(114).fill(1);
+const DECK = Array(114)
+  .fill(1)
+  .map((e, i) => (e + i < 10 ? `0${e + i}` : `${e + i}`));
 
 export function SuspectCards() {
   const [, copyToClipboard] = useCopyToClipboard();
   const cardWidth = useCardWidth();
   const [messageApi, contextHolder] = message.useMessage();
+  const qp = useQueryParams({ version: 'original' });
+  const { version = '' } = qp.queryParams;
 
   const { loading, value } = useAsync(async () => {
     const response = await fetch(`${process.env.PUBLIC_URL}/data/us/info.json`);
@@ -27,15 +32,31 @@ export function SuspectCards() {
   return (
     <Layout className="page" id="page">
       <Header className="page__header" style={{ paddingInline: 0 }}>
-        <h1>Suspects</h1> {loading} {contextHolder}
+        <h1>
+          Suspects{' '}
+          <Select
+            size="small"
+            value={version}
+            onChange={(e) => qp.add('version', e)}
+            style={{ minWidth: '10ch' }}
+          >
+            <Select.Option value="original">Original</Select.Option>
+            <Select.Option value="ai">AI</Select.Option>
+            <Select.Option value="ct">Cartoon</Select.Option>
+            <Select.Option value="alt">AI Alternative</Select.Option>
+          </Select>
+        </h1>{' '}
+        {loading} {contextHolder}
       </Header>
       <Content className="page__content suspects">
         <Image.PreviewGroup>
-          {DECK.fill(1).map((e, i) => {
-            const id = e + i < 10 ? `0${e + i}` : `${e + i}`;
+          {DECK.map((id) => {
             const entryId = `us-${id}`;
             const data = Boolean(value) ? value[entryId] : null;
-            const url = `${process.env.PUBLIC_URL}/images/us/${id}.jpg`;
+            const url =
+              version === 'original'
+                ? `${process.env.PUBLIC_URL}/images/us/${id}.jpg`
+                : `${process.env.PUBLIC_URL}/images/us/${version}/${id}.jpg`;
             const url2 = `https://www.kavispires.com/tdi/images/us/${id}.jpg`;
             return (
               <Fragment key={`img-${id}`}>
@@ -43,7 +64,7 @@ export function SuspectCards() {
                   <Image width={cardWidth} src={url} placeholder className="suspect__image" />
                   {Boolean(data) && (
                     <span className="suspect__name" role="button" onClick={() => info(url2)}>
-                      {data.gender === 'name' ? <ManOutlined /> : <WomanOutlined />}
+                      [{id}] {data.gender === 'male' ? <ManOutlined /> : <WomanOutlined />}
                       <br />
                       🇧🇷 {data.name.pt}
                       <br />
@@ -51,18 +72,6 @@ export function SuspectCards() {
                     </span>
                   )}
                 </div>
-                {/* <Image
-                    key={`img-${id}`}
-                    width={cardWidth}
-                    src={`${process.env.PUBLIC_URL}/images/td/${current}/${id}.jpg`}
-                    placeholder
-                  />
-                  <Image
-                    key={`img-${id}`}
-                    width={cardWidth}
-                    src={`${process.env.PUBLIC_URL}/images/td/${current}/${id}.jpg`}
-                    placeholder
-                  /> */}
               </Fragment>
             );
           })}
